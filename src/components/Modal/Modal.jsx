2b95f191e3be55c66a317c35ed59bf1310dde691
alexgoit <alexandrpuryaev@gmail.com>
2023-07-14 15:04:50 +0300

    Add modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,13 @@
-const Modal = () => {};
+import { Overlay, ModalWrapper } from "./Modal.styled";
+
+const Modal = ({ tags, largeImageURL }) => {
+  return (
+    <Overlay>
+      <ModalWrapper>
+        <img src={largeImageURL} alt={tags} />
+      </ModalWrapper>
+    </Overlay>
+  );
+};
 
 export default Modal;
